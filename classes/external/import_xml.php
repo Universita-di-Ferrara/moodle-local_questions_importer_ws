08f6a4349ee2fc9b9fde7f937ddcef7925c37241
BertelliAndrea <berteandrea@gmail.com>
2026-01-14 10:47:47 +0100

    init

---
+++ classes/external/import_xml.php
@@ -0,0 +1,169 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+namespace local_question_import\external;
+defined('MOODLE_INTERNAL') || die();
+
+require_once("{$CFG->libdir}/externallib.php");
+require_once("{$CFG->libdir}/questionlib.php");
+require_once("{$CFG->dirroot}/question/format/xml/format.php");
+require_once("{$CFG->dirroot}/question/editlib.php");
+require_once("{$CFG->libdir}/testing/generator/lib.php");
+
+use external_api;
+use external_function_parameters;
+use external_value;
+use external_single_structure;
+use context_course;
+use qformat_xml;
+use moodle_exception;
+use question_bank;
+
+/**
+ * Question XML Import External API
+ * @package    local_question_import
+ * @copyright  2026 Andrea Bertelli <andrea.bertelli@unife.it>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class import_xml extends external_api {
+
+    /**
+     * Descrizione dei parametri del metodo
+     * @return external_function_parameters
+     */
+    public static function execute_parameters() {
+        return new external_function_parameters([
+            'courseid' => new external_value(PARAM_INT, 'ID del corso di destinazione'),
+            'draftitemid' => new external_value(PARAM_INT, 'ID area draft contenente il file XML'),
+        ]);
+    }
+
+    /**
+     * Esegue l'importazione specifica per il formato XML
+     * @param int $courseid
+     * @param int $draftitemid
+     * @return array
+     * @throws moodle_exception
+     */
+    public static function execute(int $courseid, int $draftitemid) {
+        global $DB, $USER;
+
+        $params = self::validate_parameters(self::execute_parameters(), [
+            'courseid' => $courseid,
+            'draftitemid' => $draftitemid,
+        ]);
+
+        $context = context_course::instance($params['courseid']);
+        self::validate_context($context);
+        require_capability('local/question_import:import', $context);
+
+        $datagenerator = new \testing_data_generator();
+        $qgenerator = $datagenerator->get_plugin_generator('core_question');
+
+        $fs = get_file_storage();
+        $usercontext = \context_user::instance($USER->id);
+        $files = $fs->get_area_files($usercontext->id, 'user', 'draft', $params['draftitemid'], 'id DESC', false);
+
+        if (empty($files)) {
+            throw new moodle_exception('filenotfound', 'local_question_import');
+        }
+
+        $xmlcontent = reset($files)->get_content();
+        $qformat = new qformat_xml();
+        $questions = $qformat->readquestions(explode("\n", $xmlcontent));
+
+        $defaultcategory = question_get_default_category($context->id);
+        $currentcategoryid = $defaultcategory->id;
+
+        $transaction = $DB->start_delegated_transaction();
+        $count = 0;
+
+        try {
+            foreach ($questions as $qdata) {
+                if (isset($qdata->qtype) && $qdata->qtype === 'category') {
+                    $currentcategoryid = self::get_or_create_category($qdata->category, $context->id, $qgenerator);
+                    continue;
+                }
+
+                if (isset($qdata->qtype)) {
+                    $qdata->category = $currentcategoryid;
+                    $qdata->contextid = $context->id;
+                    $qdata->createdby = $USER->id;
+
+                    question_bank::get_qtype($qdata->qtype)->save_question($qdata, $qdata);
+                    $count++;
+                }
+            }
+            $transaction->allow_commit();
+        } catch (\Exception $e) {
+            $transaction->rollback($e);
+            throw new moodle_exception('errorimporting', 'local_question_import', '', $e->getMessage());
+        }
+
+        return [
+            'status' => true,
+            'message' => "Successfully imported $count questions from XML.",
+        ];
+    }
+
+    /**
+     * Helper per gestire la creazione ricorsiva delle categorie
+     * @param string $categorypath
+     * @param int $contextid
+     * @param \core_question_generator $qgenerator
+     * @return int ID della categoria finale
+     */
+    private static function get_or_create_category($categorypath, $contextid, $qgenerator) {
+        global $DB;
+        $categorypath = str_replace('$course$', '', $categorypath);
+        $categories = explode('/', trim($categorypath, '/'));
+        $parentid = 0;
+
+        foreach ($categories as $catname) {
+            $catname = trim($catname);
+            if (empty($catname)) {
+                continue;
+            }
+
+            $existing = $DB->get_record('question_categories', [
+                'contextid' => $contextid,
+                'name' => $catname,
+                'parent' => $parentid,
+            ]);
+
+            if ($existing) {
+                $parentid = $existing->id;
+            } else {
+                $newcat = $qgenerator->create_question_category([
+                    'name' => $catname,
+                    'contextid' => $contextid,
+                    'parent' => $parentid,
+                ]);
+                $parentid = $newcat->id;
+            }
+        }
+        return $parentid;
+    }
+
+    /**
+     * Returns the structure of the return value.
+     * */
+    public static function execute_returns() {
+        return new external_single_structure([
+            'status' => new external_value(PARAM_BOOL, 'Success'),
+            'message' => new external_value(PARAM_TEXT, 'Message'),
+        ]);
+    }
+}
---
+++ lang/en/local_question_import.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for local_question_import
+ *
+ * @package    local_question_import
+ * @copyright  2026 Andrea Bertelli <andrea.bertelli@unife.it>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'Question Web Service Import';
+$string['local_question_import:import'] = 'Import questions via Web Service';
+$string['nocategory'] = 'Could not find a valid category for this course.';
+$string['filenotfound'] = 'Draft file not found. Please upload the file first.';
+$string['question_import:import'] = 'Import questions via Web Service';
+$string['errorimporting'] = 'Error during XML import';
